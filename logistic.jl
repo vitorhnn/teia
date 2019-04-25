b1568717763b1ebef8b6496e906f984bc72cece0
Victor Hermann Chiletto <victor.vasconceloschiletto@gmail.com>
2019-04-24 22:20:46 -0300

    Consertando a regressão logística

--- logistic.jl
+++ logistic.jl
@@ -1,27 +1,25 @@
-using LinearAlgebra;
-
 g(z) = 1 ./ (1 .+ ℯ.^(-z))
 
 h(θ, x) = g(transpose(θ) * x)
 
-J(θ, x, y, m) = -sum(-y .* log.(h(θ, x)) .- ((-y) .* log.(1 .- h(θ, x)))) / m
+J(θ, x, y, m) = -sum(y .* log.(h(θ, x)) .+ (1 .- y) .* log.(1 .- h(θ, x))) / m 
 
 ∇J(θ, x, y, m) = (transpose((h(θ, x) - y) * transpose(x))) ./ m
 
 function descent(θ, α, ϵ, x, y)
     iterations = 0
     m = length(θ)
-    converged = false
-
-    while !converged
+    
+    while true
         iterations += 1
-        θ = θ - α * ∇J(θ, x, y)
+        nθ = θ - α * ∇J(θ, x, y, m)
 
-        if (J(θ, x, y, m) < ϵ)
-            converged = true
+        err = J(nθ, x, y, m)
+        if err < ϵ || isnan(err)
+            break
         end
+        θ = nθ
     end
 
-    θ, iterations
+    θ, iterations, J(θ, x, y, m)
 end
-
